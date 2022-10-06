e9d1d000d7f7ce52d0a064c8ccd42df7aaba9346
Idelio-Mata <idelioteofilomata@gmail.com>
2022-10-06 16:13:18 +0200

    First Commit

---
+++ src/main/java/com/api/rest/ifood/shopCart/ShopCartApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.rest.ifood.shopCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopCartApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopCartApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/api/rest/ifood/shopCart/model/Item.java
@@ -0,0 +1,5 @@
+package com.api.rest.ifood.shopCart.model;
+
+public class Item {
+  
+}
